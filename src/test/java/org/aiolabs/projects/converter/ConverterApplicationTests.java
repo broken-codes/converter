0475b3c5ca984e8c693b7f364695696a25b41eb9
Prashant Chaturvedi <prashant@aiolabs.org>
2022-01-08 13:31:25 +0530

    Initial commit

---
+++ src/test/java/org/aiolabs/projects/converter/ConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aiolabs.projects.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
